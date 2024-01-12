412ec0843f3be3a66c3202290cb8d97ae2c3d125
atomic0x90 <hanyujun.study@gmail.com>
2024-01-12 11:04:02 +0900

    Fix path, Add print path

--- script/SaveJSONSkillDataToFile.js
+++ script/SaveJSONSkillDataToFile.js
@@ -22,8 +22,10 @@ async function saveJSONSkillDataToFile(saveName, data, num, world_type, characte
 			// 해당 데이터 개수, 이후 JSON Data write, parsing caution
 			await fs.writeFile(path.join(directoryPath, fileName), num+'\n'+jsonData, 'utf-8');
 		}
+		const filePath = path.join(directoryPath, fileName);
+		console.log("File created at:", filePath);  // 추가된 부분
 
-		const fileContent = await fs.readFile(path.join(directoryPath, fileName), 'utf-8');
+		const fileContent = await fs.readFile(filePath, 'utf-8');
 
 		console.log(`Contents of ${fileName}:\n${fileContent}`);
 
