5770b873c2f1405b1c848ccf30f635012b0704dc
atomic0x90 <hanyujun.study@gmail.com>
2024-01-15 02:32:27 +0900

    Test OCID file del, git add OCID except

--- script/DeleteOCIDFile.js
+++ script/DeleteOCIDFile.js
@@ -1,29 +1,25 @@
 const fs = require('fs').promises;
 const path = require('path');
 
-async function initOCIDFile(){
+async function deleteOCIDFile(){
 	try{
 		const directoryPath = path.join(__dirname, 'data');
 
 		// 폴더 내 모든 파일 목록 가져오기
 		const files = await fs.readdir(directoryPath);
 
-		// 각 파일 초기화
 		await Promise.all(files.map(async (file) => {
-			if (file.includes('OCID')) {
+			if(file.includes('OCID')){
 				const filePath = path.join(directoryPath, file);
-
-				// 파일 크기를 0으로 만들어서 내용을 완전히 비우기
-				await fs.truncate(filePath, 0);
-
-				console.log(`File ${file} initialized.`);
+				await fs.unlink(filePath);
+				console.log(`File ${file} deleted.`);
 			}
 		}));
 
 	}catch (error){
-		console.error('Error initializing OCID files:', error);
+		console.error('Error delete OCID files:', error);
 	}
 }
 
-initOCIDFile();
+deleteOCIDFile();
 
